4de1e5731054aa9a18c679b91d86f61eccba48c4
Sam Devine <samuel.will.devine@gmail.com>
2021-12-14 13:21:14 -0500

    add other models, relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :merchant
 
+  has_many :invoice_items, dependent: :destroy
+  has_many :invoices, through: :invoice_items
+
   validates :name, :description, :unit_price, presence: true
   validates :unit_price, numericality: { greater_than: 0.0 }
 
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,9 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :invoices
+  has_many :transactions, through: :invoices
+  has_many :customers, through: :invoices
+  has_many :invoice_items, through: :invoices
 
   validates :name, presence: true
 
